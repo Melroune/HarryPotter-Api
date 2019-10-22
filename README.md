44f3323048ed6bf6d36911a43c373e6b597bdf56
Melroune <kerkeb@gmail.com>
2019-10-22 16:46:35 +0200

    full commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HarryPower-Api
---
+++ routes/characters.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const router = express.Router()
+
+router.get("/", (req, res) => {
+  const { id } = req.params
+  const characters = require(`../assets/json/characters.json`)
+  res.status(200).json(characters)
+})
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params
+  const characters = require(`../assets/json/id/${id}.json`)
+  res.status(200).json(characters)
+})
+
+module.exports = router
