8f90599de32f72d75f5abb4d5376f117c1c4e3f4
Melroune <kerkeb@gmail.com>
2019-10-24 20:21:59 +0200

    modif route spell v4

--- routes/spell.js
+++ routes/spell.js
@@ -1,10 +1,9 @@
 const express = require("express")
-
+const spell = require("../assets/json/spell/spell.json")
 const router = express.Router()
 
 router.get("/", async (req, res) => {
   // All spell
-  const spell = await require("../assets/json/spell/spell.json")
 
   res.status(200).json(spell)
 })
