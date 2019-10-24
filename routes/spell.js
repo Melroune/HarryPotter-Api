927d469d58f165a397129d74ede152fc9880cf98
Melroune <kerkeb@gmail.com>
2019-10-24 21:00:00 +0200

    modif route spell v7

--- routes/spell.js
+++ routes/spell.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
   // All spell
-  const spell = require("../assets/json/spell/spell.json")
-
-  res.status(200).json(spell)
+  require("../assets/json/spell/spell.json")
+    .then(spell => res.status(200).json(spell))
+    .catch(err => console.log(err))
 })
 // spell by id in progress
 // router.get("/:id", (req, res) => {
